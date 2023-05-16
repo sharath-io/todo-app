135292159facf0e5ee66a3cdc45365d53f81b114
Sharath <sharath.esr@gmail.com>
2023-05-17 00:34:33 +0530

    added sample data

---
+++ src/data/todoListItems.js
@@ -0,0 +1,29 @@
+export const TodoListItems = [
+
+    {
+        id: 1,
+        title: 'Task 1',
+        description: 'this is about task 1',
+        isCompleted: false
+    },
+    {
+        id: 2,
+        title: 'Task 2',
+        description: 'this is about task 2',
+        isCompleted: false
+    },
+    {
+        id: 1,
+        title: 'Task 3',
+        description: 'this is about task 3',
+        isCompleted: true
+    },
+    {
+        id: 1,
+        title: 'Task 4',
+        description: 'this is about task 4',
+        isCompleted: true
+    }
+]
+
+
